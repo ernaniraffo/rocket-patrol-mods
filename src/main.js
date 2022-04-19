16cb97b2e3f14fd9f17f6d1030c4529d76571c47
Ernani Raffo <eraffo@ucsc.edu>
2022-04-18 23:14:05 -0300

    comment blocks

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+// Ernani Raffo
+// Rocket Patrol Mods
+// 04/18/2022
+// The modifications took about 7 to 10 hours to implement.
+// Modifications:
+// - Displayed time in seconds on the screen (+ 10 points)
+// - Implemented particle emitter when small spaceship explodes (+ 20 points)
+// - Created new spaceship type that moves faster, is smaller, and worth more points (+ 20 points)
+// - New artwork for all in game assets (+ 20 points)
+// - Increased game timer for successful hits (+ 20 points)
+// - Allowed player to control rocket to the right and left (+ 5 points)
+// - Inscreased spaceship speed after half the original timer elapses (+ 5 points)
+// Total: 10 + 20 + 20 + 20 + 20 + 5 + 5 = 100 points
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
